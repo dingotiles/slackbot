94b77fad3b293b884591bbf526570cc621a0cad6
Dr Nic Williams <drnicwilliams@gmail.com>
2016-02-18 13:32:39 +1000

    install download robot

--- importer/importer.sh
+++ importer/importer.sh
@@ -2,14 +2,15 @@
 touch $1
 > $1
 robots=(
-    "github.com/trinchan/slackbot/robots/decide"
-    "github.com/trinchan/slackbot/robots/bijin"
-    "github.com/trinchan/slackbot/robots/nihongo"
-    "github.com/trinchan/slackbot/robots/ping"
-    "github.com/trinchan/slackbot/robots/roll"
-    "github.com/trinchan/slackbot/robots/store"
-    "github.com/trinchan/slackbot/robots/wiki"
-    "github.com/trinchan/slackbot/robots/bot"
+    "github.com/dingotiles/slackbot/robots/download"
+    # "github.com/trinchan/slackbot/robots/decide"
+    # "github.com/trinchan/slackbot/robots/bijin"
+    # "github.com/trinchan/slackbot/robots/nihongo"
+    # "github.com/trinchan/slackbot/robots/ping"
+    # "github.com/trinchan/slackbot/robots/roll"
+    # "github.com/trinchan/slackbot/robots/store"
+    # "github.com/trinchan/slackbot/robots/wiki"
+    # "github.com/trinchan/slackbot/robots/bot"
 )
 
 echo "package importer
---
+++ importer/init.go
@@ -0,0 +1,5 @@
+package importer
+
+import (
+	_ "github.com/dingotiles/slackbot/robots/download" // automatically generated import to register bot, do not change
+)
